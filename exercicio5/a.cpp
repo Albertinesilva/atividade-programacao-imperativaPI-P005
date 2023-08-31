45626d1e1e567a0d1082b831eb6f143875a28d15
João Ramos <jvitorsb98@outlook.com>
2023-08-30 22:08:30 -0300

    merge do exercicio 5

--- exercicio5/a.cpp
+++ exercicio5/a.cpp
@@ -1,13 +1,8 @@
-/*Exercício 5: Operadores bit a bit
-● Uma pesquisa monitora 8 genes de uma planta para avaliar como cada um deles influencia na resposta a uma determinada praga. Os pesquisadores optaram por representar a informação dos genes de cada planta com um inteiro sem sinal, onde cada bit da representação binária identifica se um gene está presente (1) ou não (0) naquela planta. Implemente um programa que:
-    a. Leia a informação genética de uma planta na forma de um caractere sem sinal (unsigned char);*/
-
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
+int main(){
 
     unsigned char gene;
 
